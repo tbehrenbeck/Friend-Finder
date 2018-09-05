b1a8678a512bdb6470c18e67665bfa0d4e0fe75e
Tommie Behrenbeck <behrenbt@gmail.com>
2018-09-05 09:55:25 -0500

    deploy bug

--- server.js
+++ server.js
@@ -12,14 +12,14 @@ var PORT = process.env.PORT || 3000;
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(express.static("app/public"));
 
 // Importing files
-var htmlRoutes = require("./app/routing/htmlRoutes.js");
-var apiRoutes = require("./app/routing/apiRoutes.js");
+var htmlRoutes = require("./app/routing/htmlRoutes.js")(app);
+var apiRoutes = require("./app/routing/apiRoutes.js")(app);
 
-app.use(express.static("app/public"));
-apiRoutes(app);
-htmlRoutes(app);
+// apiRoutes(app);
+// htmlRoutes(app);
 
 // Starts the server to begin listening
 // =============================================================
