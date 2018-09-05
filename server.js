610b115def884c4776a917064178809d6daaf345
Tommie Behrenbeck <behrenbt@gmail.com>
2018-09-05 14:36:43 -0500

    routes

--- server.js
+++ server.js
@@ -15,8 +15,8 @@ app.use(bodyParser.json());
 app.use(express.static("app/public"));
 
 // Importing files
-require("./app/routing/htmlRoutes.js")(app);
-require("./app/routing/apiRoutes.js")(app);
+require("./app/routes/htmlRoutes.js")(app);
+require("./app/routes/apiRoutes.js")(app);
 
 // var htmlRoutes = require("./app/routing/htmlRoutes.js")(app);
 // var apiRoutes = require("./app/routing/apiRoutes.js")(app);
